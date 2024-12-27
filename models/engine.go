98fc3ea67bfd50aec52dbbd087db0489c621837b
DON EJEH1~ <donejeh@gmail.com>
2024-12-28 00:41:33 +0100

    Engine struct

--- models/engine.go
+++ models/engine.go
@@ -1,1 +1,10 @@
 package models
+
+import "github.com/google/uuid"
+
+type Engine struct {
+	EngineID      uuid.UUID `json:"engine_id"`
+	Displacement  int64     `json:"displacement"`
+	NoOfCylinders int64     `json:"no_of_cylinders"`
+	CarRange      int64     `json:"car_range"`
+}
