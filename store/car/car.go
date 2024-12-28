ccc1782cd858f0c46aa4ac1d0cb02547f73d6602
DON EJEH1~ <donejeh@gmail.com>
2024-12-28 01:07:23 +0100

    crud method for store

--- store/car/car.go
+++ store/car/car.go
@@ -1,1 +1,37 @@
 package car
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/donejeh/car-management-system/models"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+// NewStore creates a new store
+func NewStore(db *sql.DB) Store {
+	return Store{db: db}
+}
+
+func (s Store) GetCarById(ctx context.Context, id int) (models.Car, error) {
+	// ...
+}
+
+func (s Store) GetCarByBrand(ctx context.Context, brand string, isEngine bool) {
+	// ...
+}
+
+func (s Store) CreateCar(ctx context.Context, carReq *models.CarRequest) (models.Car, error) {
+	// ...
+}
+
+func (s Store) UpdateCar(ctx context.Context, id int, carReq *models.CarRequest) (models.Car, error) {
+	// ...
+}
+
+func (s Store) DeleteCar(ctx context.Context, id int) (models.Car, error) {
+	// ...
+}
