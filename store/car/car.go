dd49bc54ff0c95182db760ef0d0aa83d1ed831d9
DON EJEH1~ <donejeh@gmail.com>
2024-12-28 11:24:22 +0100

    Add error handling and transaction support in CreateCar method

--- store/car/car.go
+++ store/car/car.go
@@ -3,8 +3,11 @@ package car
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"time"
 
 	"github.com/donejeh/car-management-system/models"
+	"github.com/google/uuid"
 )
 
 type Store struct {
@@ -58,6 +61,7 @@ func (s Store) GetCarByBrand(ctx context.Context, brand string, isEngine bool) (
 
 		if isEngine {
 			var engine models.Engine
+
 			err := rows.Scan(&car.ID, &car.Name, &car.Year, &car.Brand, &car.FuelType, &car.ID, &car.Price, &car.CreatedAt, &car.UpdatedAt, &car.Engine.EngineID, &car.Engine.Displacement, &car.Engine.NoOfCylinders, &car.Engine.CarRange)
 			if err != nil {
 				return nil, err
@@ -73,10 +77,63 @@ func (s Store) GetCarByBrand(ctx context.Context, brand string, isEngine bool) (
 
 		cars = append(cars, car)
 	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return cars, nil
 }
 
 func (s Store) CreateCar(ctx context.Context, carReq *models.CarRequest) (models.Car, error) {
-	// ...
+	var createdCar models.Car
+	var EngineID uuid.UUID
+
+	err := s.db.QueryRowContext(ctx, "SELECT id FROM engine WHERE id=$1", carReq.Engine.EngineID).Scan(&EngineID)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return createdCar, errors.New("engine not found")
+		}
+		return createdCar, err
+	}
+
+	carId := uuid.New()
+	createdAt := time.Now()
+	updatedAt := createdAt
+
+	newCar := models.Car{
+		ID:        carId,
+		Name:      carReq.Name,
+		Year:      carReq.Year,
+		Brand:     carReq.Brand,
+		FuelType:  carReq.FuelType,
+		Engine:    carReq.Engine,
+		Price:     carReq.Price,
+		CreatedAt: createdAt,
+		UpdatedAt: updatedAt,
+	}
+
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return createdCar, err
+	}
+
+	defer func() {
+		if err != nil {
+			tx.Rollback()
+			return
+		}
+		err = tx.Commit()
+	}()
+
+	query := `INSERT INTO car (id, name, year, brand, fuel_type, engine_id, price, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id, name, year, brand, fuel_type, engine_id, price, created_at, updated_at`
+
+	err = tx.QueryRowContext(ctx, query, newCar.ID, newCar.Name, newCar.Year, newCar.Brand, newCar.FuelType, newCar.Engine.EngineID, newCar.Price, newCar.CreatedAt, newCar.UpdatedAt).Scan(&createdCar.ID, &createdCar.Name, &createdCar.Year, &createdCar.Brand, &createdCar.FuelType, &createdCar.Engine.EngineID, &createdCar.Price, &createdCar.CreatedAt, &createdCar.UpdatedAt)
+
+	if err != nil {
+		return createdCar, err
+	}
+
+	return createdCar, nil
 }
 
 func (s Store) UpdateCar(ctx context.Context, id int, carReq *models.CarRequest) (models.Car, error) {
