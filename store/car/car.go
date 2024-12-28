6bb335723a602a850b6ed688f921ee7a9b200bba
DON EJEH1~ <donejeh@gmail.com>
2024-12-28 10:18:29 +0100

    Enhance car retrieval methods with detailed database queries for GetCarById and GetCarByBrand

--- store/car/car.go
+++ store/car/car.go
@@ -17,11 +17,62 @@ func NewStore(db *sql.DB) Store {
 }
 
 func (s Store) GetCarById(ctx context.Context, id int) (models.Car, error) {
-	// ...
+	var car models.Car
+
+	// Query the database
+	query := `SELECT c.id, c.name, c.year, brand , c.fuel_type, c.engine_id, c.price, c.created_at, c.updated_at, e.id, e.displacement, e.no_of_cyclinders, e.car_range FROM car c LEFT JOIN engine e ON c.engine_id = e.id WHERE c.id=$1`
+
+	row := s.db.QueryRowContext(ctx, query, id)
+	err := row.Scan(&car.ID, &car.Name, &car.Year, &car.Brand, &car.FuelType, &car.ID, &car.Price, &car.CreatedAt, &car.UpdatedAt, &car.Engine.EngineID, &car.Engine.Displacement, &car.Engine.NoOfCylinders, &car.Engine.CarRange)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return car, nil
+		}
+
+		return car, err
+	}
+
+	return car, nil
+
 }
 
-func (s Store) GetCarByBrand(ctx context.Context, brand string, isEngine bool) {
-	// ...
+func (s Store) GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error) {
+	var cars []models.Car
+	var query string
+
+	if isEngine {
+		query = `SELECT c.id, c.name, c.year, brand , c.fuel_type, c.engine_id, c.price, c.created_at, c.updated_at, e.id, e.displacement, e.no_of_cyclinders, e.car_range FROM car c LEFT JOIN engine e ON c.engine_id = e.id WHERE brand=$1`
+	} else {
+		query = `SELECT c.id, c.name, c.year, brand , c.fuel_type, c.engine_id, c.price, c.created_at, c.updated_at FROM car c WHERE brand=$1`
+	}
+
+	rows, err := s.db.QueryContext(ctx, query, brand)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var car models.Car
+
+		if isEngine {
+			var engine models.Engine
+			err := rows.Scan(&car.ID, &car.Name, &car.Year, &car.Brand, &car.FuelType, &car.ID, &car.Price, &car.CreatedAt, &car.UpdatedAt, &car.Engine.EngineID, &car.Engine.Displacement, &car.Engine.NoOfCylinders, &car.Engine.CarRange)
+			if err != nil {
+				return nil, err
+			}
+
+			car.Engine = engine
+		} else {
+			err := rows.Scan(&car.ID, &car.Name, &car.Year, &car.Brand, &car.FuelType, &car.ID, &car.Price, &car.CreatedAt, &car.UpdatedAt)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		cars = append(cars, car)
+	}
 }
 
 func (s Store) CreateCar(ctx context.Context, carReq *models.CarRequest) (models.Car, error) {
