7d1ea40a19da690c03a4402f2796b9c0d57949f5
DON EJEH1~ <donejeh@gmail.com>
2025-01-05 23:04:35 +0100

    Add CarHandler and CarServiceInterface with GetCarById method implementation

--- handler/car/car.go
+++ handler/car/car.go
@@ -1,1 +1,57 @@
 package car
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/donejeh/car-management-system/service"
+)
+
+type CarHandler struct {
+	service service.CarServiceInterface
+}
+
+func NewCarHandler(service service.CarServiceInterface) *CarHandler {
+	return &CarHandler{
+		service: service,
+	}
+}
+
+func (h *CarHandler) GetCarById(w http.ResponseWriter, r *http.Request) {
+
+	ctx := r.Context()
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	res, err := h.service.GetCarById(ctx, id)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		// http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println("Error getting car by id: ", err)
+		return
+	}
+
+	body, err := json.Marshal(res)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		// http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println("Error marshalling car by id: ", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	_, err = w.Write(body)
+
+	if err != nil {
+		log.Println("Error writing response: ", err)
+	}
+
+}
--- service/interface.go
+++ service/interface.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"context"
+
+	"github.com/donejeh/car-management-system/models"
+)
+
+type CarServiceInterface interface {
+	GetCarById(ctx context.Context, id string) (*models.Car, error)
+	GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error)
+	CreateCar(ctx context.Context, carReq *models.CarRequest) (*models.Car, error)
+	UpdateCar(ctx context.Context, id string, carReq *models.CarRequest) (*models.Car, error)
+	DeleteCar(ctx context.Context, id string) (*models.Car, error)
+}
